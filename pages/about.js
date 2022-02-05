6bf0558c2512bb8fc31ce52d548e436957d1f2a5
AtilaOliveiraJr <atila.oliveira.jr@gmail.com>
2022-02-05 14:17:37 +0100

    added call to the API

--- pages/about.js
+++ pages/about.js
@@ -4,7 +4,9 @@ import Image from 'next/image';
 export default function about() {
 	return (
         <div>
-
+            <h1>Sobre o projeto</h1>
+            <p> Descrição de como foi feito o projeto</p>
+            <Image src="/images/charizard.png" width={300} height={300} alt="Charizard image"></Image>
         </div>
     );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,32 @@
 import styles from '../styles/Home.module.css';
 
-export default function Home() {
-	return <h1>Home component</h1>;
+export async function getStaticProps() {
+	const maxPokemons = 251;
+	const api = 'https://pokeapi.co/api/v2/pokemon';
+
+	const result = await fetch(`${api}/?limit=${maxPokemons}`);
+	const data = await result.json();
+
+	data.results.map((item, index) => {
+		item.id = index + 1;
+	});
+
+	return {
+		props: {
+			pokemons: data.results,
+		},
+	};
+}
+
+export default function Home({ pokemons }) {
+	return (
+        <div>
+            <h1>Pokenext</h1>
+            <ul>
+                {pokemons.map((pokemon) => (
+                    <li key={pokemon.id}>{pokemon.name}</li>
+                ))}
+            </ul>
+        </div>
+    );
 }
