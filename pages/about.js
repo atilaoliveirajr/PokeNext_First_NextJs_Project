72107d76be95be2065032d22b3133f262db2a54e
AtilaOliveiraJr <atila.oliveira.jr@gmail.com>
2022-02-05 23:10:35 +0100

    about page refactored

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import Image from 'next/image';
 
-export default function about() {
-	return (
-        <div>
-            <h1>Sobre o projeto</h1>
-            <p> Descrição de como foi feito o projeto</p>
-            <Image src="/images/charizard.png" width={300} height={300} alt="Charizard image"></Image>
-        </div>
-    );
+import Image from 'next/image'
+
+import styles from '../styles/About.module.css'
+
+export default function About() {
+  return (
+    <div className={styles.about}>
+      <h1>About the Project</h1>
+      <p>
+        This project is for study purposes only. It was created with Nextjs and it calls the APIs at: https://pokeapi.co/docs/v2 and https://github.com/Sturlen/pokedex-pwa/wiki/API-Information.
+      </p>
+      <p>
+          Below you will find the best Pokemon ever.
+      </p>
+      <Image
+        src="/images/charizard.png"
+        width="300"
+        height="300"
+        alt="Charizard"
+      />
+    </div>
+  )
 }
